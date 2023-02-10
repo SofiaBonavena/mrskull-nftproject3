4d2d2cc320d5dc2c78f567ba6997e04af310a5fb
Sofi <sofi-b15@hotmail.com.ar>
2023-02-10 16:05:08 -0300

    instale dependencias faw y marquee y actualicé algunos componentes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
 import NavBar from '../components/NavBar/NavBar' ;
 import Hero  from "../components/Hero/Hero";
-// import Collection from "../components/Collection/Collection";
+import Collection from "../components/Collection/Collection";
 import About from "../components/About/About";
 import Mint from '../components/Mint/Mint';
-// import Faq from '../components/Faq/Faq' ;
+import Faq from '../components/Faq/Faq' ;
 
 const Home = () => {
   return (
     <>
       <NavBar/>
       <Hero />
-      {/* <Collection /> */}
+      <Collection />
       <Mint />
       <About />
-      {/* <Faq /> */}
+      <Faq />
       
     </>
   );
